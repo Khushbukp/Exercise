56a3e11ea7aded89e60b3eec09c764849ef5145a
khushbu <khushbu.patel@clairvoyantsoft.com>
2019-03-29 15:43:23 -0700

    unwanted script

--- hive_execution.sh
+++
@@ -1,9 +0,0 @@
-## Importing the utility function from file Exercise_Utility_Functions which contains method called validate_execution
-
-. /home/cloudera/heena/Exercise_utility_function
-
-## executing hiveql.hql
-hive -f /home/cloudera/heena/hiveql.hql 
-validate_execution $? 
-
-
